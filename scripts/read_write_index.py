245b8997fd52110efdb72240534608ce2703f49f
Tom Power <thomasedwardpower@gmail.com>
2024-03-09 19:29:32 -0800

    Create read_write_index.py
    read write python script

---
+++ scripts/read_write_index.py
@@ -0,0 +1,20 @@
+# Import the csv module
+import csv
+
+# Open the text file and the CSV file
+text_file = open('text_file.txt', 'r')
+csv_file = open('csv_file.csv', 'w', newline='')
+
+# Create a csv.writer object
+writer = csv.writer(csv_file)
+
+# Read the text file line by line and write to the CSV file
+for line in text_file:
+    # Split the line by whitespace to get the fields
+    fields = line.split()
+    # Write the fields as a row to the CSV file
+    writer.writerow(fields)
+
+# Close the files
+text_file.close()
+csv_file.close()
